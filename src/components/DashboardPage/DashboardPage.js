f02846e564bd07594ea602656d92fa749e4f95ed
mudman28 <johnnie.walters@gmail.com>
2019-06-24 16:45:14 -0500

    dashboard boxes

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
-
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import './DashboardPage.css';
+import FaceIcon from '@material-ui/icons/Face';
 class DashboardPage extends Component {
 render () {
     return (
-        <p>WELCOME TO DASHBOARD</p>
+        <div>
+        <Grid container>
+        <Grid item sm>
+          <Paper style={{ padding: 40, marginTop: 20, marginLeft: 100 }}>
+            <h2>Today's Task</h2>
+              <Card className="todayBox">
+                    <CardActionArea>
+                        <CardMedia>
+                            
+                        </CardMedia>
+                    </CardActionArea>
+                </Card>
+          </Paper>
+        </Grid>
+        <Grid item sm>
+          <Paper style={{ padding: 40, marginTop: 20 }}>
+          <FaceIcon className="profileIcon" />
+            <h2>Job Requirements</h2>
+            
+            <Card className="requireBox">
+                    <CardActionArea>
+                        <CardMedia>
+                            
+                        </CardMedia>
+                    </CardActionArea>
+                </Card>
+          </Paper>
+        </Grid>
+      </Grid>
+      </div>
     )
 }
 }
