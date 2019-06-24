83f2ca0c78a8c003581c23b123de81d24e847f63
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-06-24 14:53:56 -0500

    installed-virtualized and created the table

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import DashboardTable from '../DashboardTable/DashboardTable';
 
 class DashboardPage extends Component {
 render () {
     return (
+        <div>
         <p>WELCOME TO DASHBOARD</p>
+        <p> Job Pipeline + </p>
+                  <div>
+                   <table>
+                       <tbody>
+                         <tr>
+                       <th>Company</th>
+                         <th>Position</th>
+                         <th>Stage</th>
+                         <th>Next Touch Point</th>
+                         <th>Notes</th>
+                         </tr>
+                         </tbody>
+                         </table>
+                         </div>
+                         <DashboardTable />
+                         </div>
+
     )
 }
 }
