a26fd89207a5406eed1bbb0f11527d658dd35db1
Meyer <ehlmeyer@gmail.com>
2019-06-25 11:44:27 -0500

    GET works in postman for logged in user

--- server/routes/requirement.router.js
+++ server/routes/requirement.router.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-
+//rejectUnauthenticated verifies user is logged in before displaying data
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('in GET /api/requirement. user:', req.user.id)
+    const queryText = 'SELECT * FROM "requirements" WHERE "user_id"=$1'
+    pool.query(queryText,[req.user.id])
+        .then(result=>{
+            console.log('back from GET /api/requirement', result.rows)
+            res.send(result.rows)
+        })
+        .catch(err=>{
+            console.log('error in GET /api/requirement', err)
+            res.sendStatus(500)
+        })
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
     console.log('in POST /api/requirement', req.body, req.user.id)
     const queryText='INSERT INTO "requirements" (requirement, user_id) VALUES ($1,$2)'
     pool.query(queryText, [req.body.requirement, req.user.id])
