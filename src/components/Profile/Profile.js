74fd56c08798996b72e2a11b9bb234ba82c81c14
mudman28 <johnnie.walters@gmail.com>
2019-06-25 14:04:32 -0500

    created the shell for the Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 
-import Grid from '@material-ui/core/Grid';
 import FaceIcon from '@material-ui/icons/Face';
+import AddIcon from '@material-ui/icons/Add';
 import './Profile.css';
 
 class Profile extends Component {
-render () {
-    return (
-        <div>
-            <h1>Profile</h1>
-            <div className="profileBox">
-                <FaceIcon className="profileIcon" />
-                <div className="profileForm">
-                    <p>First Name:</p>
-                    <p>Last Name:</p>
-                    <p>E-mail:</p>
+    render() {
+        return (
+            <div>
+                <h1>Profile</h1>
+                <div className="profileBox">
+                    <FaceIcon className="profileIcon" />
                 </div>
+                <form className="profileForm">
+                    <div>
+
+                        <p>First Name:   <input type="text" placeholder="First Name" /></p>
+                        <p>Last Name: <input type="text" placeholder="Last Name" /></p>
+                        <p>E-mail: <input type="text" placeholder="E-mail" /></p>
+                        <p><AddIcon className="profileAddIcon" /> Requirement</p>
+
+                    </div>
+                </form>
             </div>
-          
-        </div>
-    )
-}
+        )
+    }
 }
 
 export default Profile;
