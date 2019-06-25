8b4be9de5f5fb1a69db406fd61ff987568b796bb
mudman28 <johnnie.walters@gmail.com>
2019-06-24 23:43:43 -0500

    corrected the dash

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia';
 import './DashboardPage.css';
 import FaceIcon from '@material-ui/icons/Face';
 import DashboardTable from '../DashboardTable/DashboardTable';
@@ -16,36 +13,43 @@ render () {
 
         <Grid container>
         <Grid item sm>
-          <Paper style={{ padding: 40, marginTop: 20, marginLeft: 100 }}>
-            <h2>Today's Task</h2>
-              <Card className="todayBox">
-                    <CardActionArea>
-                        <CardMedia>
-                            
-                        </CardMedia>
-                    </CardActionArea>
-                </Card>
+          <Paper style={{ padding: 40, marginTop: 20}}>
+            <h1>Today's Task</h1>
+              <div className="todayBox">
+                    <ul className="boxText">
+                        <li>Call Karen</li>
+                        <li>Apply to 30 jobs by 7/20</li>
+                        <li>Redo my resumes</li>
+                        <li>Call Karen</li>
+                        <li>Apply to 30 jobs by 7/20</li>
+                        <li>Redo my resumes</li>
+                        <li>Call Karen</li>
+                        <li>Apply to 30 jobs by 7/20</li>
+                        <li>Redo my resumes</li>
+                        <li>Call Karen</li> 
+                    </ul>
+                </div>
           </Paper>
         </Grid>
         <Grid item sm>
           <Paper style={{ padding: 40, marginTop: 20 }}>
           <FaceIcon className="profileIcon" />
-            <h2>Job Requirements</h2>
-            
-            <Card className="requireBox">
-                    <CardActionArea>
-                        <CardMedia>
-                            
-                        </CardMedia>
-                    </CardActionArea>
-                </Card>
+            <h1>Job Requirements</h1>
+            <div className="requireBox">
+                <ul className="boxText">
+                    <li>6 weeks vacation</li>
+                    <li>Work from home</li>
+                    <li>75k anually</li>
+                </ul>
+            </div>
           </Paper>
         </Grid>
       </Grid>
+      <DashboardTable />
       </div>
 
 //         <p>WELCOME TO DASHBOARD</p>
-//         <DashboardTable />
+//         
 //         </div>
 
 
