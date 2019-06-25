8dbc87ba76575dac067baa9bc6fd7569fec0ce88
mudman28 <johnnie.walters@gmail.com>
2019-06-25 16:50:25 -0500

    Changed the profile inputs but the add requirement button won't create inputs on the next line

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
 import FaceIcon from '@material-ui/icons/Face';
 import AddIcon from '@material-ui/icons/Add';
 import './Profile.css';
 
 class Profile extends Component {
+    state = {
+        profileInfo: {
+            first_name: '',
+            last_name: '',
+            email: '',
+        },
+        requireList: [
+            {requirement: ''}
+        ]
+    }
+
+    addRequirement(){
+        this.setState({requireList: [...this.state.requireList, '']})
+    }
+
+    handleChange = propertyName => (event) => {
+        console.log('new', event.target.value);
+        this.setState({
+            profileInfo: {
+            ...this.state.profileInfo,
+            [propertyName]: event.target.value
+          }
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
@@ -12,13 +42,45 @@ class Profile extends Component {
                 <div className="profileBox">
                     <FaceIcon className="profileIcon" />
                 </div>
-                <form className="profileForm">
-                    <div>
-
-                        <p>First Name:   <input type="text" placeholder="First Name" /></p>
-                        <p>Last Name: <input type="text" placeholder="Last Name" /></p>
-                        <p>E-mail: <input type="text" placeholder="E-mail" /></p>
-                        <p><AddIcon className="profileAddIcon" /> Requirement</p>
+                <form className="profileForm" onSubmit={this.handleSubmit}>
+                    <div>       
+                        <TextField
+                            id="standard-dense"
+                            label="First Name"
+                            className="profileInput"
+                            margin="dense" 
+                            onChange={this.handleChange('first_name')} />
+                            <br />
+                        <TextField
+                            id="standard-dense"
+                            label="Last Name"
+                            className="profileInput"
+                            margin="dense" 
+                            onChange={this.handleChange('last_name')} />
+                            <br />
+                        <TextField
+                            id="filled-email-input"
+                            label="E-mail"
+                            className="profileInput"
+                            type="email"
+                            name="email"
+                            autoComplete="email"
+                            margin="dense" 
+                            onChange={this.handleChange('email')}/>
+                           
+                        {
+                            this.state.requireList.map((requirement, index) =>{
+                                return(
+                                        <TextField
+                                        id="standard-dense"
+                                        label="Requirement"
+                                        className="profileInput"
+                                        margin="dense" 
+                                        onChange={this.handleChange('requirement')} />
+                                )
+                            })
+                        }
+                        <p ><AddIcon className="profileAddIcon" onClick={(event) => this.addRequirement(event)} /> Requirement</p>
 
                     </div>
                 </form>
@@ -27,4 +89,7 @@ class Profile extends Component {
     }
 }
 
-export default Profile;
+const mapStateToProps = state => ({
+    pro: state.userReducer,
+  });
+  export default connect(mapStateToProps)(Profile);
