b601ce8a23bde839c07461aecb979d7ab0457e8d
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-06-28 14:44:53 -0500

    made some query change

--- src/components/DashboardTable/DashboardTable.js
+++ src/components/DashboardTable/DashboardTable.js
@@ -2,143 +2,26 @@ import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import MuiVirtualizedTable from './MuiVirtualizedTable';
 import {connect} from 'react-redux';
-// import PropTypes from 'prop-types';
-// import clsx from 'clsx';
-// import { withStyles } from '@material-ui/core/styles';
-// import TableCell from '@material-ui/core/TableCell';
-
-// import { AutoSizer, Column, Table } from 'react-virtualized';
 
 
-// const styles = theme => ({
-//   flexContainer: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     boxSizing: 'border-box',
-//   },
-//   tableRow: {
-//     cursor: 'pointer',
-//   },
-//   tableRowHover: {
-//     '&:hover': {
-//       backgroundColor: theme.palette.grey[200],
-//     },
-//   },
-//   tableCell: {
-//     flex: 1,
-//   },
-//   noClick: {
-//     cursor: 'initial',
-//   },
-// });
+// const sample = [
+//   ['Okta', 'Customer Service', 'Phone Screen', '06/05/19', 'Katie is calling at 9:30am'],
+//   ['Microsoft', 'System Analyst', 'Final', '06/06/19', 'Jeff will make the decision'],
+//   ['Ubisoft', 'Business analyst', 'Computer Engineer', '06/08/19', 'Bring tax documents'],
+//   ['Prime Academy', 'Outreach', 'In-person', '06/10/19', 'Meet in Minneapollis'],
+//   ['Sezzle', 'Test Analyst', 'HR Interview', '06/15/19', 'Followup with HR'],
+// ];
 
-// class MuiVirtualizedTable extends React.PureComponent {
-//   static defaultProps = {
-//     headerHeight: 48,
-//     rowHeight: 48,
-//   };
-
-//   getRowClassName = ({ index }) => {
-//     const { classes, onRowClick } = this.props;
-
-//     return clsx(classes.tableRow, classes.flexContainer, {
-//       [classes.tableRowHover]: index !== -1 && onRowClick != null,
-//     });
-//   };
-
-//   cellRenderer = ({ cellData, columnIndex }) => {
-//     const { columns, classes, rowHeight, onRowClick } = this.props;
-//     return (
-//       <TableCell
-//         component="div"
-//         className={clsx(classes.tableCell, classes.flexContainer, {
-//           [classes.noClick]: onRowClick == null,
-//         })}
-//         variant="body"
-//         style={{ height: rowHeight }}
-//         align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
-//       >
-//         {cellData}
-//       </TableCell>
-//     );
-//   };
-
-//   headerRenderer = ({ label, columnIndex }) => {
-//     const { headerHeight, columns, classes } = this.props;
-
-//     return (
-//       <TableCell
-//         component="div"
-//         className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
-//         variant="head"
-//         style={{ height: headerHeight }}
-//         align={columns[columnIndex].numeric || false ? 'right' : 'left'}
-//       >
-//         <span>{label}</span>
-//       </TableCell>
-//     );
-//   };
-
-//   render() {
-//     const { classes, columns, ...tableProps } = this.props;
-//     return (
-//       <AutoSizer>
-//         {({ height, width }) => (
-//           <Table height={height} width={width} {...tableProps} rowClassName={this.getRowClassName}>
-//             {columns.map(({ dataKey, ...other }, index) => {
-//               return (
-//                 <Column
-//                   key={dataKey}
-//                   headerRenderer={headerProps =>
-//                     this.headerRenderer({
-//                       ...headerProps,
-//                       columnIndex: index,
-//                     })
-//                   }
-//                   className={classes.flexContainer}
-//                   cellRenderer={this.cellRenderer}
-//                   dataKey={dataKey}
-//                   {...other}
-//                 />
-//               );
-//             })}
-//           </Table>
-//         )}
-//       </AutoSizer>
-//     );
-//   }
+// function createData(id, company, position, stage, nexttouchpoint, notes) {
+//   return { id, company, position, stage, nexttouchpoint, notes};
 // }
-
-// MuiVirtualizedTable.propTypes = {
-//   classes: PropTypes.object.isRequired,
-//   columns: PropTypes.arrayOf(PropTypes.object).isRequired,
-//   headerHeight: PropTypes.number,
-//   onRowClick: PropTypes.func,
-//   rowHeight: PropTypes.number,
-// };
-
-// const DashboardTable = withStyles(styles)(MuiVirtualizedTable);
-
-//---
-
-const sample = [
-  ['Okta', 'Customer Service', 'Phone Screen', '06/05/19', 'Katie is calling at 9:30am'],
-  ['Microsoft', 'System Analyst', 'Final', '06/06/19', 'Jeff will make the decision'],
-  ['Ubisoft', 'Business analyst', 'Computer Engineer', '06/08/19', 'Bring tax documents'],
-  ['Prime Academy', 'Outreach', 'In-person', '06/10/19', 'Meet in Minneapollis'],
-  ['Sezzle', 'Test Analyst', 'HR Interview', '06/15/19', 'Followup with HR'],
-];
-
-function createData(id, company, position, stage, nexttouchpoint, notes) {
-  return { id, company, position, stage, nexttouchpoint, notes};
-}
 
 const rows = [];
 
-for (let i = 0; i < 50; i += 1) {
-  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  rows.push(createData(i, ...randomSelection));
-}
+// for (let i = 0; i < 50; i += 1) {
+//   const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+//   rows.push(createData(i, ...randomSelection));
+// }
 
 class DashboardTable extends Component {
 
@@ -149,7 +32,7 @@ componentDidMount() {
   return (
     <Paper style={{ height: 335, width: '100%' }}>
       <MuiVirtualizedTable
-        rowCount={rows.length}
+         rowCount={rows.length}
         rowGetter={({ index }) => rows[index]}
         columns={[
           {
