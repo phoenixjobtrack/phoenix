ce34ee52405713b4665770840b06b0872977628b
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-06-28 16:01:16 -0500

    added some data in the table

--- src/components/DashboardTable/DashboardTable.js
+++ src/components/DashboardTable/DashboardTable.js
@@ -4,24 +4,24 @@ import MuiVirtualizedTable from './MuiVirtualizedTable';
 import {connect} from 'react-redux';
 
 
-// const sample = [
-//   ['Okta', 'Customer Service', 'Phone Screen', '06/05/19', 'Katie is calling at 9:30am'],
-//   ['Microsoft', 'System Analyst', 'Final', '06/06/19', 'Jeff will make the decision'],
-//   ['Ubisoft', 'Business analyst', 'Computer Engineer', '06/08/19', 'Bring tax documents'],
-//   ['Prime Academy', 'Outreach', 'In-person', '06/10/19', 'Meet in Minneapollis'],
-//   ['Sezzle', 'Test Analyst', 'HR Interview', '06/15/19', 'Followup with HR'],
-// ];
+const sample = [
+  ['Okta', 'Customer Service', 'Phone Screen', '06/05/19', 'Katie is calling at 9:30am'],
+  ['Microsoft', 'System Analyst', 'Final', '06/06/19', 'Jeff will make the decision'],
+  ['Ubisoft', 'Business analyst', 'Computer Engineer', '06/08/19', 'Bring tax documents'],
+  ['Prime Academy', 'Outreach', 'In-person', '06/10/19', 'Meet in Minneapollis'],
+  ['Sezzle', 'Test Analyst', 'HR Interview', '06/15/19', 'Followup with HR'],
+];
 
-// function createData(id, company, position, stage, nexttouchpoint, notes) {
-//   return { id, company, position, stage, nexttouchpoint, notes};
-// }
+function createData(id, company, position, stage, nexttouchpoint, notes) {
+  return { id, company, position, stage, nexttouchpoint, notes};
+}
 
 const rows = [];
 
-// for (let i = 0; i < 50; i += 1) {
-//   const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-//   rows.push(createData(i, ...randomSelection));
-// }
+for (let i = 0; i < 50; i += 1) {
+  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+  rows.push(createData(i, ...randomSelection));
+}
 
 class DashboardTable extends Component {
 
