6a8c92eac21739dbcb50ef683fed5a8263ab02b8
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-06-28 10:11:31 -0500

    able to GET all the required function for the job pipeline table for dashboard view

--- server/routes/jobs.router.js
+++ server/routes/jobs.router.js
@@ -2,13 +2,13 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+
+//-----GET for job pipeline table-------//
 router.get('/', (req,res) => {
     console.log('this is for job', req.user.id);
-    // let query = `SELECT * FROM "jobs"
-    // WHERE "user_id" = $1 `;
     let query = `SELECT jobs.company_name, jobs.position, stages.stage, 
-    stages.date, stages.note FROM "jobs","stages"
-      WHERE jobs.id = stages.job_id and;`
+    stages.date, stages.note FROM "jobs"  JOIN "stages" ON 
+    jobs.id = stages.job_id WHERE "user_id"=$1;`
     pool.query(query,[req.user.id])
         .then( (result) => {
             res.send(result.rows);
