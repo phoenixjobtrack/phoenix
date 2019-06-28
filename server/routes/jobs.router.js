4313bf625f941364f0ee1e9ae7bfdee4651f6452
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-06-28 09:03:34 -0500

    GET for job is working

--- server/routes/jobs.router.js
+++ server/routes/jobs.router.js
@@ -4,8 +4,11 @@ const router = express.Router();
 
 router.get('/', (req,res) => {
     console.log('this is for job', req.user.id);
-    let query = `SELECT * FROM "jobs"
-    WHERE "user_id" = $1 `;
+    // let query = `SELECT * FROM "jobs"
+    // WHERE "user_id" = $1 `;
+    let query = `SELECT jobs.company_name, jobs.position, stages.stage, 
+    stages.date, stages.note FROM "jobs","stages"
+      WHERE jobs.id = stages.job_id and;`
     pool.query(query,[req.user.id])
         .then( (result) => {
             res.send(result.rows);
