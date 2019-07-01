58fa7f6749b497ba21158d5fdd31997ed2eeb317
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-07-01 10:22:03 -0500

    able to show the jobs in the job pipeline table

--- src/components/DashboardTable/DashboardTable.js
+++ src/components/DashboardTable/DashboardTable.js
@@ -4,31 +4,38 @@ import MuiVirtualizedTable from './MuiVirtualizedTable';
 import {connect} from 'react-redux';
 
 
-const sample = [
-  ['Okta', 'Customer Service', 'Phone Screen', '06/05/19', 'Katie is calling at 9:30am'],
-  ['Microsoft', 'System Analyst', 'Final', '06/06/19', 'Jeff will make the decision'],
-  ['Ubisoft', 'Business analyst', 'Computer Engineer', '06/08/19', 'Bring tax documents'],
-  ['Prime Academy', 'Outreach', 'In-person', '06/10/19', 'Meet in Minneapollis'],
-  ['Sezzle', 'Test Analyst', 'HR Interview', '06/15/19', 'Followup with HR'],
-];
+// const sample = [
+//   ['Okta', 'Customer Service', 'Phone Screen', '06/05/19', 'Katie is calling at 9:30am'],
+//   ['Microsoft', 'System Analyst', 'Final', '06/06/19', 'Jeff will make the decision'],
+//   ['Ubisoft', 'Business analyst', 'Computer Engineer', '06/08/19', 'Bring tax documents'],
+//   ['Prime Academy', 'Outreach', 'In-person', '06/10/19', 'Meet in Minneapollis'],
+//   ['Sezzle', 'Test Analyst', 'HR Interview', '06/15/19', 'Followup with HR'],
+// ];
 
 function createData(id, company, position, stage, nexttouchpoint, notes) {
   return { id, company, position, stage, nexttouchpoint, notes};
 }
 
-const rows = [];
 
-for (let i = 0; i < 50; i += 1) {
-  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  rows.push(createData(i, ...randomSelection));
-}
+
+
 
 class DashboardTable extends Component {
+
+  
+
 
 componentDidMount() {
     this.props.dispatch({ type: 'FETCH_JOBS'})
 }
+
   render(){
+    // for (let i = 0; i < 50; i += 1) {
+    //   const randomSelection = this.props.reduxState.jobs[Math.floor(Math.random() * this.props.reduxState.jobs.length)];
+    //   //  rows.push(createData(i, ...randomSelection));
+    // }
+ const rows = this.props.reduxState.jobs;
+
   return (
     <Paper style={{ height: 335, width: '100%' }}>
       <MuiVirtualizedTable
@@ -38,7 +45,7 @@ componentDidMount() {
           {
             width: 250,
             label: 'Company',
-            dataKey: 'company',
+            dataKey: 'company_name',
           },
           {
             width: 250,
@@ -55,13 +62,13 @@ componentDidMount() {
           {
             width: 250,
             label: 'Next Activity Date',
-            dataKey: 'nexttouchpoint',
+            dataKey: 'date',
             numeric: true,
           },
           {
             width: 600,
             label: 'Notes',
-            dataKey: 'notes',
+            dataKey: 'note',
             //numeric: true,
           },
         ]}
