a3792bb6511c5b301142b9a1690125a4e68c16b7
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-07-02 11:48:36 -0500

    clean up sidebar code

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -10,7 +10,6 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
@@ -21,17 +20,9 @@ import './SideMenu.css';
 // ========== MUI ICONS ========== //
 import AccountIcon from '@material-ui/icons/AccountCircle';
 import ContactsIcon from '@material-ui/icons/Contacts'
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import FolderIcon from '@material-ui/icons/Folder';
 import HomeIcon from '@material-ui/icons/Home';
-import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import InfoIcon from '@material-ui/icons/Info';
 import ListAltIcon from '@material-ui/icons/ListAlt'
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
 import ViewAgendaIcon from '@material-ui/icons/ViewAgenda';
 
 const drawerWidth = 240;
@@ -103,10 +94,6 @@ function SideMenu(props) {
                 </List>
                 <Divider />
             </Drawer>
-            {/* <main className={classes.content}>
-                <div className={classes.toolbar} />
-                
-            </main> */}
         </div>
     );
 }
