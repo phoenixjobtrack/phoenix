3a8095888f12dcac64485ed442f41ade7c01ffc1
Meyer <ehlmeyer@gmail.com>
2019-07-07 18:28:03 -0500

    styled buttons on pipeline, fixed date format

--- src/components/JobPipelinePage/NewTable.js
+++ src/components/JobPipelinePage/NewTable.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment'
+
+import {Button, IconButton, Tooltip} from '@material-ui/core'
+import OpenInNewIcon from '@material-ui/icons/OpenInNew'
+import DeleteIcon from '@material-ui/icons/DeleteOutline'
+
+
 import './NewTable.css';
 
 class NewTable extends Component {
@@ -26,11 +33,25 @@ class NewTable extends Component {
                                         <td >{job.company_name}</td>
                                         <td>{job.position}</td>
                                         <td>{job.currentStage}</td>
-                                        <td>{job.nextStageDate.substr(0, 16)}</td>
+                                        <td>{job.nextStageDate && moment(job.nextStageDate).format('MM-DD-YYYY')}</td>
                                         <td>{job.nextStageNote}</td>
                                         <td>{job.nextstage}</td>
-                                        <td><button>Update</button></td>
-                                        <td><button>Delete</button></td>
+                                        <td>
+                                            <Tooltip title="View more and edit">
+                                                <IconButton variant="contained" color="primary">
+                                                    <OpenInNewIcon aria-label="view more and edit"/>
+                                                </IconButton>
+                                            </Tooltip>
+                                            
+                                        </td>
+                                        <td>
+                                            <Tooltip title="Remove job">
+                                                <IconButton >
+                                                    <DeleteIcon color="secondary" />
+                                                </IconButton>
+                                            </Tooltip>
+                                        </td> 
+                                            {/* <Button variant="contained" color="secondary">Delete</Button></td> */}
                                         {/* <td><button className="update-button" value={event.id} onClick={() => this.updateEvent(event.id)}>Update</button></td>
                                         <td><button className="delete-button" value={event.id} onClick={() => this.deleteEvent(event.id)}>Delete</button></td> */}
                                     </tr>
