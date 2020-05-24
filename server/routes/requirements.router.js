1abbabe72f89c5dcccbbe36c9ed7affcc9dec04f
Dexter St Pierre <dexter.stpierre.dev@gmail.com>
2020-05-24 11:03:37 -0500

    Update requirements and tasks routers to use ORM

--- server/routes/requirements.router.js
+++ server/routes/requirements.router.js
@@ -1,59 +1,81 @@
-const express = require('express');
-const pool = require('../modules/pool');
-const { rejectUnauthenticated } = require('../modules/authentication-middleware');
-const router = express.Router();
+const express = require('express')
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware')
+const { Requirement } = require('../schemas')
+
+const router = express.Router()
 
 //rejectUnauthenticated verifies user is logged in before displaying data
 
 // // GET route
 router.get('/', rejectUnauthenticated, (req, res) => {
-    const queryText = `SELECT * FROM "requirements" WHERE "user_id"=$1 ORDER BY "id" ASC;`
-    pool.query(queryText,[req.user.id])
-        .then(result=>{
-            res.send(result.rows)
-        })
-        .catch(err=>{
-            res.sendStatus(500)
-        })
-});
-
+  Requirement.findAll({
+    where: {
+      userId: req.user.id,
+    },
+  })
+    .then((requirements) => {
+      res.send(requirements)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.sendStatus(500)
+    })
+})
 
 // POST route
 router.post('/', rejectUnauthenticated, (req, res) => {
-    const queryText='INSERT INTO "requirements" (requirement, user_id) VALUES ($1,$2)'
-    pool.query(queryText, [req.body.requirement, req.user.id])
-        .then(response=>{
-            res.sendStatus(201)
-        })
-        .catch(err=>{
-            res.sendStatus(500)
-        })
-});
+  Requirement.create({
+    requirement: req.body.requirement,
+    userId: req.user.id,
+  })
+    .then((requirement) => {
+      res.send(requirement)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.sendStatus(500)
+    })
+})
 
 //PUT route
-router.put('/:id', (req,res)=>{
-    const queryText ='UPDATE "requirements" SET "requirement"=$1 WHERE "id"=$2 AND "user_id"=$3;';
-    pool.query(queryText, [req.body.requirement, req.params.id, req.user.id])
-        .then(response=>{
-            res.sendStatus(200);
-        })
-        .catch(err=>{
-            console.log('error in PUT /api/requirements', err)
-            res.sendStatus(500)
-        })    
+router.put('/:id', (req, res) => {
+  Requirement.update(
+    {
+      requirement: req.body.requirement,
+    },
+    {
+      where: {
+        userId: req.user.id,
+        id: req.params.id,
+      },
+    },
+  )
+    .then((requirement) => {
+      res.send(requirement)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.sendStatus(500)
+    })
 })
 
 //DELETE route
-router.delete('/:id', rejectUnauthenticated, (req,res)=>{
-    console.log('in DELETE /api/requirements', req.params.id, req.user.id)
-    const queryText='DELETE FROM "requirements" WHERE "id"=$1 AND "user_id"=$2'
-    pool.query(queryText,[req.params.id, req.user.id])
-        .then(response=>{
-            res.sendStatus(200)
-        })
-        .catch(err=>{
-            res.sendStatus(500)
-        })
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+  Requirement.destroy({
+    where: {
+      id: req.params.id,
+      userId: req.user.id,
+    },
+  })
+    .then(() => {
+      res.sendStatus(204)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.sendStatus(500)
+    })
 })
 
-module.exports = router;
+module.exports = router
